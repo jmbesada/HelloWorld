a56d887c9d360848fb8d83bdf7a984f232b0cab5
jmbesada <jmbesada@dev.alphatool.es>
2012-06-13 11:29:44 +0200

    Import initial

---
+++ src/main/java/com/qwi/josemi/roo/domain/Login.java
@@ -0,0 +1,17 @@
+package com.qwi.josemi.roo.domain;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Login {
+
+	public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+}
